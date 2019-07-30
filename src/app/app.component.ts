79f81b4b46ba237d29f1fbd4976edb608ff51493
Alexander Konov <a.a.konov@mail.ru>
2019-07-30 13:43:52 +0300

    project configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'pl-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'petrol';
+
+  public title = 'petrol';
+
 }
